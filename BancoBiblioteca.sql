b1b33ab96c79bf1aaff7581f3f74e0bfe3901796
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-01-30 22:39:37 -0300

    Upgrade banco Biblioteca

--- BancoBiblioteca.sql
+++ BancoBiblioteca.sql
@@ -9,3 +9,14 @@ CONSTRAINT pk_id_autor PRIMARY KEY(IdAutor)
 
 --Procedimento armazenadoque trás informações sobre a tabela.
 sp_help Autor;
+
+CREATE TABLE Editora(
+IdEditora SMALLINT PRIMARY KEY IDENTITY,
+NomeEditora VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE Assunto(
+IdAssunto TINYINT PRIMARY KEY IDENTITY,
+NomeAssunto VARCHAR(25) NOT NULL
+);
